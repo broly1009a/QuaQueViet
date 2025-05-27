0548f4725b2ce9017950fab27c1f815b2315cd73
devs <hoanghiep310102@gmail.com>
2025-05-27 19:56:26 +0700

    config databse1

--- QuaQueViet/src/java/dal/DBContext.java
+++ QuaQueViet/src/java/dal/DBContext.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 
 public class DBContext {
     public Connection getConnection() throws Exception {
-        String url = "jdbc:sqlserver://fpt-sql-server.database.windows.net:1433;database=ShopYouAndMeVersionFinal2;user=hiepdevs@fpt-sql-server;password=Taovipko0@;encrypt=true;trustServerCertificate=true;loginTimeout=30;";
+        String url = "jdbc:sqlserver://fpt-sql-server.database.windows.net:1433;database=ShopYouAndMeVersionFinal2;user=hiepdevs@fpt-sql-server;password=Taovipko0@;encrypt=true;trustServerCertificate=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         return DriverManager.getConnection(url);
     }
