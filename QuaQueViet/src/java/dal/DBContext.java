79fff618e28135c7d835b3b873a2e52ee16447a6
devs <hoanghiep310102@gmail.com>
2025-05-27 19:40:41 +0700

    config databse

--- QuaQueViet/src/java/dal/DBContext.java
+++ QuaQueViet/src/java/dal/DBContext.java
@@ -1,40 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dal;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-
 public class DBContext {
-    
-    /*USE BELOW METHOD FOR YOUR DATABASE CONNECTION FOR BOTH SINGLE AND MULTILPE SQL SERVER INSTANCE(s)*/
- /*DO NOT EDIT THE BELOW METHOD, YOU MUST USE ONLY THIS ONE FOR YOUR DATABASE CONNECTION*/
     public Connection getConnection() throws Exception {
-        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName;
-        if (instance == null || instance.trim().isEmpty()) {
-            url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
-        }
+        String url = "jdbc:sqlserver://fpt-sql-server.database.windows.net:1433;database=ShopYouAndMeVersionFinal2;user=hiepdevs@fpt-sql-server;password=Taovipko0@;encrypt=true;trustServerCertificate=true;loginTimeout=30;";
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        return DriverManager.getConnection(url, userID, password);
+        return DriverManager.getConnection(url);
     }
-    private final String serverName = "localhost";
-    private final String dbName = "ShopYouAndMeVersionFinal2";
-    private final String portNumber = "1433";
-    private final String instance = "";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
-    private final String userID = "sa";
-    private final String password = "123";
-    
 }
-class Using{
+
+class Using {
     public static void main(String[] args) {
         try {
             new DBContext().getConnection();
-            System.out.println("Ket noi thanh cong");
+            System.out.println("Kết nối thành công");
         } catch (Exception e) {
-            System.out.println("Ket noi that bai" + e.getMessage());
+            System.out.println("Kết nối thất bại: " + e.getMessage());
         }
     }
-    }
+}
